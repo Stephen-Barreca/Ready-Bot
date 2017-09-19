11665742523f42e3f1af38ad162a0a4111341429
Stephen <stephen@ready-robotics.com>
2017-09-19 09:51:35 -0400

    initial setup for creating slack reports

--- src/slackmate.py
+++ src/slackmate.py
@@ -6,14 +6,15 @@ from responses import lunch_responses, lunch_keywords
 from slackclient import SlackClient
 
 # constants
-BOT_ID = os.environ.get('bot_id')
-TOKEN = os.environ.get('token')
+BOT_ID = os.environ.get('bot_id', 'U5CM069UH')
+TOKEN = os.environ.get('token', 'xoxb-182714213969-zTOYpuGNt7XcL8zy1PsBpqGW')
 AT_BOT = "<@{}>".format(BOT_ID)
 EXAMPLE_COMMAND = "do"
 BOT_NAME = 'slackmate'
-COMMANDS = ['do', 'what', 'calculate']
+COMMANDS = ['do', 'what', 'calculate', 'report']
 KEYWORDS = lunch_keywords
 KEYWORD_TRIGGER_PERCENT = 100
+READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
 
 slack_client = SlackClient(TOKEN)
 
@@ -31,6 +32,8 @@ def handle_command(command, channel):
         response = "Sure...write some more code then I can do that!"
     elif command.startswith('calculate'):
         response = "Sure...write some more code then I can do that!"
+    elif command.startswith('report'):
+        response = "The code is being worked on right now"
 
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
@@ -41,16 +44,19 @@ def handle_keyword(message, channel):
     """
     message = message.lower()
     response = None
-    for lunch_key in lunch_keywords:
-        if lunch_key in message:
+    for KEYWORD in KEYWORDS:
+        if KEYWORD in message:
             trigger_roll = random.randint(0, 99)
             if trigger_roll < KEYWORD_TRIGGER_PERCENT:
                 response = random.SystemRandom().choice(lunch_responses)
             else:
-                print('The almighty Rand Don has blocked this message')
+                print('The almighty Ran Dom has blocked this message')
 
     if response:
         slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+
+def handle_report(command, channel):
+    pass
 
 def parse_slack_output(slack_rtm_output):
     """
@@ -62,8 +68,10 @@ def parse_slack_output(slack_rtm_output):
     #print(output_list)
     if output_list and len(output_list) > 0:
         for output in output_list:
+            #print('found :: {}'.format(output))
             if output and 'text' in output and 'bot_id' not in output:
                 if AT_BOT in output['text']:
+                    print('found :: {}'.format(output))
                     # return text after the @ mention, whitespace removed
                     return 'command', output['text'].split(AT_BOT)[1].strip().lower(), output['channel'], output['user']
                 else:
@@ -73,9 +81,9 @@ def parse_slack_output(slack_rtm_output):
     return None, None, None, None
 
 if __name__ == "__main__":
-    READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("Slackmate connected and running!")
+        print(AT_BOT)
         api_call = slack_client.api_call("users.list")
         if api_call.get('ok'):
             # retrieve all users so we can find our bot
@@ -85,13 +93,16 @@ if __name__ == "__main__":
                     print("Bot ID for '{}' is {}".format(user['name'], user.get('id')))
         else:
             print("could not find bot user with the name {}".format(BOT_NAME))
+
         while True:
             trigger, command, channel, user = parse_slack_output(slack_client.rtm_read())
             if trigger and channel:
-                if trigger == 'keyword':
+                if trigger.lower() == 'keyword':
                     handle_keyword(command, channel)
-                elif trigger == 'command':
+                elif trigger.lower() == 'command':
                     handle_command(command, channel)
+
+
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
